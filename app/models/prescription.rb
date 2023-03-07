e609d58de450c41faad6a561d334e905e8234bc6
Theophile Thiberge <tthiberge@gmail.com>
2023-03-07 16:52:29 +0100

    Basis of the seeds with prescriptions

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -2,5 +2,5 @@ class Prescription < ApplicationRecord
   belongs_to :user
   belongs_to :treatment
 
-  validates :start_date, :end_date, :time, presence: true
+  validates :start_date, :end_date, :todo_hours, :todo_minutes, presence: true
 end
