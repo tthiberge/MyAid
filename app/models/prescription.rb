72f043a8f45f9a37599d307042afb46fe94a601b
Theophile Thiberge <tthiberge@gmail.com>
2023-03-07 17:39:48 +0100

    Defining methods to do .pills .cares and .exercises on a user instance

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,4 +3,5 @@ class Prescription < ApplicationRecord
   belongs_to :treatment
 
   validates :start_date, :end_date, :todo_hours, :todo_minutes, presence: true
+
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -4,4 +4,16 @@ class Treatment < ApplicationRecord
   validates :name, :category, presence: true
   CATEGORIES = %w(pills cares exercises)
   validates :category, inclusion: { in: CATEGORIES }
+
+  def self.pills
+    where(category: "pills")
+  end
+
+  def self.cares
+    where(category: "cares")
+  end
+
+  def self.exercises
+    where(category: "exercises")
+  end
 end
