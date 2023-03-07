dce1ecdd44a2b2af27ab6c188af3a964bb75f7bd
Theophile Thiberge <tthiberge@gmail.com>
2023-03-07 13:06:47 +0100

    Initializing the computing of the GPS coordinates through address for doctors and users examples

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,7 @@ class Doctor < ApplicationRecord
   has_many :appointments
 
   validates :name, :specialty, :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
