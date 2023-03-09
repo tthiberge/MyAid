987559f562e28be9be0606c238f8fd288dd05472
diane lesvenan <diane_lesvenan@icloud.com>
2023-03-09 12:38:42 +0100

    added the favicon

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,9 @@ Rails.application.routes.draw do
   # get "", to: "appointments#confirm"
   resources :appointments, only: [ :index, :show, :new, :create, :edit, :update, :destroy]
   resources :diaries, only: [ :index, :show, :new, :create]
+
+
   get "/uikit", to: "pages#uikit"
+  get "/uikitbis", to: "pages#uikitbis"
+  get "/uikitter", to: "pages#uikitter"
 end
