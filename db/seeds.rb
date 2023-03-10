a0fc00da942655e8c83b92e40f87c685a72bbebc
Theophile Thiberge <tthiberge@gmail.com>
2023-03-10 13:19:28 +0100

    starting seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,7 +65,7 @@ maung = User.create!(
 )
 
 # Creating the prescriptions
-prescription_1 = Prescription.create!(start_date: "10/03/2023", end_date: "20/03/2023", todo_hours: 12, todo_minutes: 30, comment: "2 x 500mg - Take the pills before the main meal of the day", user: theo, treatment: paracetamol)
+prescription_1 = Prescription.create!(start_date: "10/03/2023", end_date: "20/03/2023", todo_hours: 12, todo_minutes: 30, day_half: "AM" comment: "2 x 500mg - Take the pills before the main meal of the day", user: theo, treatment: paracetamol)
 
 prescription_2 = Prescription.create!(start_date: "07/03/2023", end_date: "17/03/2023", todo_hours: 21, todo_minutes: 00, comment: "3 x 125mg - Take the pills before going to sleep", user: theo, treatment: tylenol)
 
