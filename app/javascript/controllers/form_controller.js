30c2183b44e2cce1095131522b6c61f832ac5464
Theophile Thiberge <tthiberge@gmail.com>
2023-03-12 16:20:00 +0100

    transofmring the treatments UI in the prescription formulaire

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,9 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
 
-  static target = ["dropdown"]
+  static target = ["dropdown", "formulaire"]
 
   connect() {
     console.log("test")
   }
+
+  Reveal() {
+    // ça fonctionne puisque j'ai reussi à afficher dans la console event.currentTarget
+    // ça a l'air d'être ma dropdownTarget qu'il n'arrive pas à sélectionner
+    console.log(this.formulaireTarget)
+    this.formulaireTarget.classList.add("bk-success")
+  }
+
 }
