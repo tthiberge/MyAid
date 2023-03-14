1ba151ebd390eacfef796b2486bd0cfbc8fceedf
Joy Danenberg <joy.danenberg@gmail.com>
2023-03-14 17:58:36 +0100

    cloudinary boost colors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_090424) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_142816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,6 +94,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_090424) do
     t.string "day_half"
     t.index ["treatment_id"], name: "index_prescriptions_on_treatment_id"
     t.index ["user_id"], name: "index_prescriptions_on_user_id"
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.string "author"
+    t.string "motivational_quote"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "selfies", force: :cascade do |t|
