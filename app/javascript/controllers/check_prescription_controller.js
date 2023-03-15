eb64574ff45da731f45fd50ee817346c1d09b6fa
Theophile Thiberge <tthiberge@gmail.com>
2023-03-15 15:53:22 +0100

    fixing form prescriptions

--- app/javascript/controllers/check_prescription_controller.js
+++ app/javascript/controllers/check_prescription_controller.js
@@ -9,19 +9,19 @@ export default class extends Controller {
     // aucun rapport avec la category du treatment
     }
 
-  connect() {
-    console.log("check-prescription")
-    // console.log(this.categoryValue) // syntaxe JS. Toutes les values se font appeler comme ça
-    // console.log(this.element.value) // la valeur du label avec les logos
+  // connect() {
+  //   console.log("check-prescription")
+  //   // console.log(this.categoryValue) // syntaxe JS. Toutes les values se font appeler comme ça
+  //   // console.log(this.element.value) // la valeur du label avec les logos
 
-    // Si le nom du label (avec logo)
-    if (this.element.value.toUpperCase().includes(this.categoryValue.toUpperCase())) {
-      // this.element.checked = true // ça, ça ne faisait que selectionner en violet, mais moi j'avais mis de toggle d-none au CLICK
-      // Paul dit qu'on peut SIMULER LE CLICK!
-      this.element.click()
-    }
+  //   // Si le nom du label (avec logo)
+  //   if (this.element.value.toUpperCase().includes(this.categoryValue.toUpperCase())) {
+  //     // this.element.checked = true // ça, ça ne faisait que selectionner en violet, mais moi j'avais mis de toggle d-none au CLICK
+  //     // Paul dit qu'on peut SIMULER LE CLICK!
+  //     this.element.click()
+  //   }
 
 
 
-  }
+  // }
 }
