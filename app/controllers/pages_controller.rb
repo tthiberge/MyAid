c171adb48fdc43e2be44b114969ce64ab770f8f0
diane lesvenan <diane_lesvenan@icloud.com>
2023-03-15 09:44:17 +0100

    deleted calendars method in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -125,16 +125,5 @@ class PagesController < ApplicationController
   def boost
     @quote = Quote.all.sample
   end
-
-  def calendars
-    @appointments = current_user.appointments
-    @pill_prescriptions = current_user.prescriptions.joins(:treatment).where(treatments: { category: "pills" })
-    @care_prescriptions = current_user.prescriptions.joins(:treatment).where(treatments: { category: "cares" })
-    @exercise_prescriptions = current_user.prescriptions.joins(:treatment).where(treatments: { category: "exercises" })
-
-    @events = @appointments + @pill_prescriptions + @care_prescriptions + @exercise_prescriptions
-
-    @start_date = Date.current.beginning_of_month
-    @end_date = Date.current.end_of_month
-  end
+  
 end
