8e1487c0489b8817a8aa313f0ca64ec445dbf4a9
mmkclement <maungmaungkha18290@gmail.com>
2023-03-15 17:43:22 +0100

    sweet alert for confirm and delete

--- app/javascript/controllers/alert_confirm_care_controller.js
+++ app/javascript/controllers/alert_confirm_care_controller.js
@@ -2,13 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 import Swal from 'sweetalert2'
 
 
-// Connects to data-controller="alert-confirm"
+// Connects to data-controller="alert-confirm-care"
 export default class extends Controller {
   connect() {
     Swal.fire({
       position: 'top-end',
       icon: 'success',
-      title: 'I met my Doctor',
+      title: 'Great! you finished care',
       showConfirmButton: false,
       timer: 2500
     })
