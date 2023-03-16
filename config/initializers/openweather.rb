def9e9da69cf140402cf46fbd7ed4ab6fe6f11eb
mmkclement <maungmaungkha18290@gmail.com>
2023-03-16 12:57:31 +0100

    solve localhost

--- config/initializers/openweather.rb
+++ config/initializers/openweather.rb
@@ -3,21 +3,21 @@
 require 'open-uri'
 require 'net/https'
 require 'openssl'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
 
-OpenWeather.configure do |config|
-  config.api_key = ENV['OPENWEATHER_API_KEY']
-  config.adapter = Faraday.default_adapter
-  config.http_adapter.ssl.verify = true
-end
+# OpenWeather.configure do |config|
+#   config.api_key = ENV['OPENWEATHER_API_KEY']
+#   config.adapter = Faraday.default_adapter
+#   config.http_adapter.ssl.verify = true
+# end
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
 
-OpenWeather.configure do |config|
-  config.api_key = ENV['OPENWEATHER_API_KEY']
-  config.adapter = Faraday.default_adapter
-  config.http_adapter.ssl.verify = true
-end
+# OpenWeather.configure do |config|
+#   config.api_key = ENV['OPENWEATHER_API_KEY']
+#   config.adapter = Faraday.default_adapter
+#   config.http_adapter.ssl.verify = true
+# end
 
 
 module Net
