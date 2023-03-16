b02119d2d6780e5dc4bd50711d2318f1e2527344
mmkclement <maungmaungkha18290@gmail.com>
2023-03-16 15:27:33 +0100

    I am with sunny

--- config/initializers/openweather.rb
+++ config/initializers/openweather.rb
@@ -12,15 +12,17 @@ require 'openssl'
 # end
 
 
+if ENV["IS_HEROKU"]
 
-module Net
-  class HTTP
-    alias_method :original_use_ssl=, :use_ssl=
+  module Net
+    class HTTP
+      alias_method :original_use_ssl=, :use_ssl=
 
-    def use_ssl=(flag)
-      self.ca_file = "/usr/lib/ssl/certs/ca-certificates.crt"
-      self.verify_mode = OpenSSL::SSL::VERIFY_PEER
-      self.original_use_ssl = flag
+      def use_ssl=(flag)
+        self.ca_file = "/usr/lib/ssl/certs/ca-certificates.crt"
+        self.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        self.original_use_ssl = flag
+      end
     end
   end
 end
